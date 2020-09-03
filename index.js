10c5b830c1a61bdc1b96edfc6f2be52938286196
ayhand <ayhandani@hotmail.com>
2020-09-03 22:17:15 +0200

    almoooost

--- index.js
+++ index.js
@@ -1,33 +1,149 @@
 $(document).ready(function () {
-	let inner$ = $("#inner");
-	let outer$ = $("#outer");
+	let $main = $("#main");
+	let $button = $("#getButton");
+	let $text = $("#text");
+	let $ridol = $("#ridol");
+	let $solblur = $("#solblur");
+	let $sol = $("#sol");
+	let $images = $("#images");
+	let $sapphire = $("#sapphire");
 
-	let ow = 400;
-	let oh = 400;
-
-	function zoom(x, y, w, h) {
-		let zoomX = ow * 1.0 / w;
-		let zoomY = oh * 1.0 / h;
-
-		let dx = -x * zoomX;
-		let dy = -y * zoomY;
-
-		inner$.css({
-			"background-size": `${zoomX * 100}% ${zoomY * 100}%`,
-			"background-position": `${dx}px ${dy}px`
-		});
+	function createRidolStep(text) {
+		return () => {
+			$main.children().hide();
+			$text.show();
+			$text.text(text);
+			$images.show();
+			$ridol.show();
+			$sol.show();
+			$solblur.show();
+		}
 	}
 
-	zoom(240, 192, 6, 6);
+	function createSolStep(text) {
+		return () => {
+			$main.children().hide();
+			$text.show();
+			$text.text(text);
+			$images.show();
+			$ridol.show();
+			$sol.show();
+			$solblur.show();
+			$ridol.css({
+				"opacity": 0
+			})
+		}
+	}
 
-	setTimeout(() => {
-		let durationEasing = "2s ease-out"
-		inner$.css({
-			"transition": `background-size ${durationEasing}, background-position ${durationEasing}`,
-		});
-	
-		setTimeout(() => {
-			zoom(100, 100, 200, 200);
-		}, 1000);
-	}, 10);
-})
+	function createSolBlurStep(text) {
+		return () => {
+			$main.children().hide();
+			$text.show();
+			$text.text(text);
+			$images.show();
+			// $sol.show();
+			$solblur.show();
+
+			// let zoom = 1;
+			let zoom = 456.0 / 7;
+			let w = 1985 * zoom;
+			let h = 2394 * zoom;
+			let x = 216 - zoom * 806
+			let y = 299 - zoom * 1505
+			$sol.css({
+				"opacity": 0
+			});
+			$solblur.css({
+				"background-size": `${w}px ${h}px`,
+				"background-position": `${x}px ${y}px`,
+			});
+		}
+	}
+
+	function createFinalStep(text) {
+		return () => {
+			$main.children().hide();
+			$text.show();
+			$text.text(text);
+			$images.show();
+			// $sol.show();
+			$solblur.show();
+
+			// let zoom = 1;
+			let zoom = 0.5;
+			let w = 1985 * zoom;
+			let h = 2394 * zoom;
+			$sol.css({
+				"opacity": 0
+			});
+			let durationEasing = "2s ease";
+			$solblur.css({
+				"transition": `background-size ${durationEasing}, background-position ${durationEasing}`,
+				"background-size": `${w}px ${h}px`,
+				"background-position": `-87px -153.5px`,
+			})
+		}
+	}
+
+	function createSapphireStep() {
+		return () => {
+			$main.children().hide();
+			$text.show();
+			$text.text("MaMa, aZ Én SzEmEm Is OlYaN fÉnYeSeN cSiLlOg, MiNt A SzAfÍr™?");
+			$images.show();
+			$sapphire.show();
+		}
+	}
+
+	let steps = [
+		"wata happend to the tatato? 🍉",
+		"wel helo ma nam iz Loooca an eym a speshul pag 🍉",
+		"nyam nyam sajt, sonka, puha kenyérke, bacon, csoki, süti, CSOKI, MÉG TÖBB CSOKI CSOKIIIIIIIII!! (elnézést, kicsit elragadott a hév) 🍉",
+		"tengerpart, feggőágy, SZUNDI! 🍉",
+		"tengerpart, feggőágy, SZUNDI! 🍉",
+		"oooh luk a vargagyuláné! 🍉",
+		"Hopá!",
+		"Ez valami új?",
+		"Piszony!",
+		"Készültem egy pindurka meglepetéssel!",
+		"De előtte",
+		"Elárulok egy titokt!",
+		createRidolStep("Emlékszel erre?"),
+		createRidolStep("Ez egy részlet egy képből!"),
+		createSolStep("Ez egy részlet egy képből!"),
+		createSolStep("Hogy mit ábrázol a kép?"),
+		"Egy titkot™!",
+		":O",
+		"Egy olyan titkot™ aminek én nagyon örültem.",
+		"Annyira, hogy nem tudtam egy kicsit nem elmondani.",
+		"Ezért csináltam belőle egy ridolt™! Amit ugyan nem lehetett megfejteni, de egy kicsit elújságoztam neked a titkot™!",
+		"Most viszont meg is mutatom a megfejtést™!",
+		createSolStep("Redeh?"),
+		createSolBlurStep("Luk!"),
+		createFinalStep("Dis!"),
+		createFinalStep("U no wut dis iz?"),
+		createFinalStep("Dis iz a..."),
+		createSapphireStep(),
+		"És az én ujjacskáim™",
+		"És az én zafírom™!",
+		"Amit neked™ akarok adni!",
+		"De ahhoz meg kell ígérj nekem valamit™!",
+		"Tudod mit?",
+		"Azt, hogy hozzám jössz feleségül™! 🍉",
+		"The End!",
+		""
+	];
+
+	let idx = -1;
+	$button.click(() => {
+		idx++;
+		let step = steps[idx]
+		if (typeof (step) === "string") {
+			$images.hide();
+			$text.text(step).show();
+		}
+		if (typeof (step) === "function") {
+			step()
+		}
+	});
+});
